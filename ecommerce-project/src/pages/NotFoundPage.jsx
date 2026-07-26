3e6d1f5d1dc1a4357fae6a572c1056f57765833f
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-07-26 20:00:40 +0800

    6k - Create Not Found Page

---
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -0,0 +1,17 @@
+import './NotFoundPage.css';
+import { Header } from '../components/Header';
+
+export function NotFoundPage() {
+    return (
+        <>
+            <title>404 Not Found</title>
+            <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+
+            <Header />
+
+            <div className="not-found-message">
+                Page Not Found
+            </div>
+        </>
+    );
+}
