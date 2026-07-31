28e03d1068d1708cf4b683c37e08a9b32aaad8b5
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-07-31 12:23:03 +0800

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,6 +5,14 @@ import { products } from "../../starting-code/data/products";
 import CheckMarkIcon from "../assets/images/icons/checkmark.png";
 
 export function HomePage() {
+
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      return response.json()
+    }).then((data) => {
+        console.log(data);
+    });
+
   return (
     <>
       <title>Home</title>
