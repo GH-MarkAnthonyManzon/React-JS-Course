c84faea6bc68df8525deb512b609323a8d61c298
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-07-31 12:52:19 +0800

    Use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,16 +1,16 @@
-import "./HomePage.css";
-import { Header } from "../components/Header";
-import { products } from "../../starting-code/data/products";
+import axios from 'axios';
 
-import CheckMarkIcon from "../assets/images/icons/checkmark.png";
+import './HomePage.css';
+import { Header } from '../components/Header';
+import { products } from '../../starting-code/data/products';
+
+import CheckMarkIcon from '../assets/images/icons/checkmark.png';
 
 export function HomePage() {
 
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      return response.json()
-    }).then((data) => {
-        console.log(data);
+      console.log(response.data);
     });
 
   return (
