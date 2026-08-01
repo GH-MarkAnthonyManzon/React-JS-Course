f255785afe644d1b742371ef83500e9c49328d9d
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-08-01 12:37:40 +0800

    Add server proxy config for /api and /images

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -14,13 +14,13 @@ export function HomePage() {
   useEffect(() => {
     
     // Fetch products
-    axios.get('http://localhost:3000/api/products')
+    axios.get('/api/products')
       .then((response) => {
         setProducts(response.data);
       });
 
     // Fetch cart items
-    axios.get('http://localhost:3000/api/cart-items')
+    axios.get('/api/cart-items')
       .then((response) => {
         setCart(response.data);
       });
