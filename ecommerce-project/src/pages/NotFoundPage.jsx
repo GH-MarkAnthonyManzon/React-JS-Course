f3903e0202244b1b06aae42ede1b82b10fd2f1ea
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-08-08 00:08:51 +0800

    7o - Create a cart prop Not Found Page and pass it inside Header component and its route

--- ecommerce-project/src/pages/NotFoundPage.jsx
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -1,13 +1,13 @@
 import './NotFoundPage.css';
 import { Header } from '../components/Header';
 
-export function NotFoundPage() {
+export function NotFoundPage({ cart }) {
     return (
         <>
             <title>404 Not Found</title>
             <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
 
-            <Header />
+            <Header cart={cart} />
 
             <div className="not-found-message">
                 Page Not Found
