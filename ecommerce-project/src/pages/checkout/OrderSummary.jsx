7626e0747d20df1903eb5a08991cf7a74339fb46
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-08-10 11:26:41 +0800

    Create delete cart item feature

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,24 +1,27 @@
+import { DeliveryOptions } from "./DeliveryOptions";
+import { CartItemDetails } from "./CartItemDetails";
+import { DeliveryDate } from "./DeliveryDate";
 
-import { DeliveryOptions } from './DeliveryOptions';
-import { CartItemDetails } from './CartItemDetails';
-import { DeliveryDate } from './DeliveryDate';
-
-export function OrderSummary({ cart, deliveryOptions, loadCart }) {
+export function OrderSummary({ cart, deliveryOptions, loadCart, deleteCartItem }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 &&
         cart.map((cartItem) => {
-          
           return (
             <div key={cartItem.productId} className="cart-item-container">
-
-              <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions} />
+              <DeliveryDate
+                cartItem={cartItem}
+                deliveryOptions={deliveryOptions}
+              />
 
               <div className="cart-item-details-grid">
+                <CartItemDetails cartItem={cartItem} loadCart={loadCart} deleteCartItem={deleteCartItem} />
 
-                <CartItemDetails cartItem={cartItem} />
-
-                <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
+                <DeliveryOptions
+                  cartItem={cartItem}
+                  deliveryOptions={deliveryOptions}
+                  loadCart={loadCart}
+                />
               </div>
             </div>
           );
