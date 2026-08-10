cf568636f2e3e6afc54cd6001352762aa7a9191d
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-08-10 12:34:30 +0800

    8a - Create add to cart feature in orders page

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -5,17 +5,16 @@ import "./OrdersPage.css";
 import { Header } from "../../components/Header";
 import { OrdersGrid } from "./OrdersGrid";
 
-export function OrdersPage({ cart }) {
+export function OrdersPage({ cart, loadCart }) {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     const fetchOrdersData = async () => {
-      const response = await axios.get("/api/orders?expand=products")
+      const response = await axios.get("/api/orders?expand=products");
       setOrders(response.data);
-    }
+    };
 
     fetchOrdersData();
-
   }, []);
 
   return (
@@ -28,8 +27,7 @@ export function OrdersPage({ cart }) {
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
 
-        <OrdersGrid orders={orders} />
-
+        <OrdersGrid orders={orders} loadCart={loadCart} />
       </div>
     </>
   );
