a7d369ec2e4d3213d389709ee00e3f645e9efd9b
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-08-10 12:49:32 +0800

    8b - Fetch payment summary in a separate useEffect

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -13,16 +13,20 @@ export function CheckoutPage({ cart, loadCart }) {
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
-      let response = await axios.get(
+      const response = await axios.get(
         "/api/delivery-options?expand=estimatedDeliveryTime",
       );
       setDeliveryOptions(response.data);
+    };
+    fetchCheckoutData();
+  }, []);
 
-      response = await axios.get("/api/payment-summary");
+  useEffect(() => {
+    const fetchPaymentSummary = async () => {
+      const response = await axios.get("/api/payment-summary");
       setPaymentSummary(response.data);
     };
-
-    fetchCheckoutData();
+    fetchPaymentSummary();
   }, [cart]);
 
   return (
