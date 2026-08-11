8fac2b0326c37608d558bb1551fb4e3a95118e69
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-08-11 14:39:25 +0800

    8f - Make update quantity in checkout page work

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -2,7 +2,7 @@ import { DeliveryOptions } from "./DeliveryOptions";
 import { CartItemDetails } from "./CartItemDetails";
 import { DeliveryDate } from "./DeliveryDate";
 
-export function OrderSummary({ cart, deliveryOptions, loadCart, deleteCartItem }) {
+export function OrderSummary({ cart, deliveryOptions, loadCart }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 &&
@@ -15,7 +15,7 @@ export function OrderSummary({ cart, deliveryOptions, loadCart, deleteCartItem }
               />
 
               <div className="cart-item-details-grid">
-                <CartItemDetails cartItem={cartItem} loadCart={loadCart} deleteCartItem={deleteCartItem} />
+                <CartItemDetails cartItem={cartItem} loadCart={loadCart} />
 
                 <DeliveryOptions
                   cartItem={cartItem}
