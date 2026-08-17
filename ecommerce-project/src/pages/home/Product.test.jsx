d398cf9f52454e0b203f1c09d2ce1a4b6557e030
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-08-17 15:46:08 +0800

    9f - Move userEvent.setup() to beforeEach and share user object

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -9,6 +9,7 @@ vi.mock('axios');
 describe('Product Component', () => {
     let product;
     let loadCart;
+    let user;
 
     beforeEach(() => {
         product = {
@@ -24,6 +25,7 @@ describe('Product Component', () => {
         };
 
         loadCart = vi.fn();
+        user = userEvent.setup();
     });
 
     it('displays the product details correctly', () => {
@@ -53,7 +55,6 @@ describe('Product Component', () => {
     it('adds a product to the cart', async () => {
         render(<Product product={product} loadCart={loadCart} />);
 
-        const user = userEvent.setup();
         const addToCartButton = screen.getByTestId('add-to-cart-button');
         await user.click(addToCartButton);
 
@@ -74,7 +75,6 @@ describe('Product Component', () => {
         const quantitySelector = screen.getByTestId('product-quantity-selector');
         expect(quantitySelector).toHaveValue('1');
 
-        const user = userEvent.setup();
         await user.selectOptions(quantitySelector, '3');
         expect(quantitySelector).toHaveValue('3');
 
