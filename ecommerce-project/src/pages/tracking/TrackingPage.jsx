fb0a2dfc745755653df320740dc1176d3220f8a3
GH-MarkAnthonyManzon <manzonmark.ph@gmail.com>
2026-08-20 22:08:25 +0800

    Move tracking page files to /tracking

--- ecommerce-project/src/pages/tracking/TrackingPage.jsx
+++ ecommerce-project/src/pages/tracking/TrackingPage.jsx
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { Link } from "react-router";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
 import "./TrackingPage.css";
 
 export function TrackingPage({ cart }) {
